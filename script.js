cf57513cbac9a60f9ac12b74971decfaa9292c78
anuragstomar <anuragstomar121@gmail.comssh-keygen -t ed25519 -C anuragstomar121@gmail.com>
2024-02-10 13:34:34 +0530

    js_test

---
+++ script.js
@@ -0,0 +1,32 @@
+//reverse a string
+// const reverseString = (Name) => {
+//     let reverseName=""
+//     for (let i=Name.length-1; i>=0;i--){
+//         reverseName=reverseName+Name[i]
+//     }
+//     console.log(reverseName);
+// };
+// reverseString("sahil");
+
+
+
+//find vowels
+// function vovels()  
+// {
+//     let Name= "anuragsinghtomar";
+
+//     let op="";
+//     for (let i=0;i<=Name.length-1;i++)
+//     {
+//         if("a"==Name[i] || "e"==Name[i] || "i"==Name[i] || "o"==Name[i] || "u"==Name[i])
+//         {
+//             op += Name[i];
+
+//         }
+//     }
+//     console.log(op);
+// }
+
+// vovels();
+
+
